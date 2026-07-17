7878084a31439e8a97f32ece42cd168e9d74d9c4
Aggsx <132386971+Aggsx@users.noreply.github.com>
2026-07-17 16:17:03 +0000

    chore(jopdf): bump de hash remoto

--- flake.nix
+++ flake.nix
@@ -5,28 +5,30 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      systems = [ "x86_64-linux" ];
-      forEachSystem = nixpkgs.lib.genAttrs systems;
-    in {
-      nixosModules = import ./modules;
-      lib = import ./lib { inherit nixpkgs; };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = ["x86_64-linux"];
+    forEachSystem = nixpkgs.lib.genAttrs systems;
+  in {
+    nixosModules = import ./modules;
+    lib = import ./lib {inherit nixpkgs;};
 
-      packages = forEachSystem (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in {
-          jopdf = pkgs.callPackage ./pkgs/jopdf/package.nix {};
-          default = self.packages.${system}.jopdf;
-        }
-      );
+    packages = forEachSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        jopdf = pkgs.callPackage ./pkgs/jopdf/package.nix {};
+        default = self.packages.${system}.jopdf;
+      }
+    );
 
-      # LA SOLUCIÓN REAL: Usamos "${self}/..." para fijar la ruta absoluta del repositorio
-      overlays.default = final: prev: {
-        jopdf = final.callPackage "${self}/pkgs/jopdf/package.nix" {};
-        # Cuando agregues más paquetes en el futuro, los pones aquí usando ${self} igual:
-        # otro-paquete = final.callPackage "${self}/pkgs/otro-paquete/package.nix" {};
-      };
+    # LA SOLUCIÓN REAL: Usamos "${self}/..." para fijar la ruta absoluta del repositorio
+    overlays.default = final: prev: {
+      jopdf = final.callPackage "${self}/pkgs/jopdf/package.nix" {};
+      # Cuando agregues más paquetes en el futuro, los pones aquí usando ${self} igual:
+      # otro-paquete = final.callPackage "${self}/pkgs/otro-paquete/package.nix" {};
     };
+  };
 }
