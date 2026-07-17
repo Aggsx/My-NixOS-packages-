d8738f69fddbe6649d10102b4ac28bea3bef5e90
Aggsx <axelrdnz@gmail.com>
2026-07-17 11:26:48 -0600

    fix(jopdf): añadir autoPatchelfHook y dependencias faltantes

--- pkgs/jopdf/package.nix
+++ pkgs/jopdf/package.nix
@@ -13,6 +13,10 @@
   fontconfig,
   freetype,
   libglvnd,
+  autoPatchelfHook,
+  zlib,
+  e2fsprogs,
+  libgpg-error,
 }:
 stdenv.mkDerivation rec {
   pname = "jopdf";
@@ -23,41 +27,53 @@ stdenv.mkDerivation rec {
     hash = "sha256-G993GJOUOh6WsbXcxir1MKrsUFmqCfqA4BtuAyKMsyc=";
   };
 
-  nativeBuildInputs = [dpkg makeWrapper qt5.wrapQtAppsHook];
-  buildInputs = [qt5.qtbase libsForQt5.qtstyleplugins libxcb libxkbcommon libx11 libxext fontconfig freetype libglvnd];
+  nativeBuildInputs = [ dpkg makeWrapper qt5.wrapQtAppsHook autoPatchelfHook ];
 
-  dontAutoPatchelf = true;
+  buildInputs = [
+    stdenv.cc.cc.lib
+    qt5.qtbase
+    libsForQt5.qtstyleplugins
+    libxcb
+    libxkbcommon
+    libx11
+    libxext
+    fontconfig
+    freetype
+    libglvnd
+    zlib
+    e2fsprogs
+    libgpg-error
+  ];
+
   unpackPhase = "dpkg-deb -x $src .";
 
   installPhase = ''
-        runHook preInstall
+    runHook preInstall
 
-        mkdir -p $out/opt $out/bin $out/share/applications $out/share/icons/hicolor/256x256/apps
+    mkdir -p $out/opt $out/bin $out/share/applications $out/share/icons/hicolor/256x256/apps
 
-        # Copiar los archivos binarios principales
-        cp -r opt/jopdf $out/opt/
+    # Copiar los archivos binarios principales
+    cp -r opt/jopdf $out/opt/
 
-        # Crear el wrapper con las variables de entorno necesarias
-        makeWrapper $out/opt/jopdf/JOPDF $out/bin/jopdf \
-          --set QT_QPA_PLATFORMTHEME gtk3 \
-          --set GTK_USE_PORTAL 1 \
-          --prefix QT_PLUGIN_PATH : "${libsForQt5.qtstyleplugins}/lib/qt-5/plugins" \
-          --prefix LD_LIBRARY_PATH : "$out/opt/jopdf/lib:${lib.makeLibraryPath [stdenv.cc.cc.lib libglvnd]}"
+    # Crear el wrapper con las variables de entorno necesarias
+    makeWrapper $out/opt/jopdf/JOPDF $out/bin/jopdf \
+      --set QT_QPA_PLATFORMTHEME gtk3 \
+      --set GTK_USE_PORTAL 1 \
+      --prefix QT_PLUGIN_PATH : "${libsForQt5.qtstyleplugins}/lib/qt-5/plugins"
 
-        # Extracción forzada del ícono: busca en las rutas comunes desempacadas o en el directorio opt
-        if [ -f usr/share/pixmaps/jopdf.png ]; then
-          cp usr/share/pixmaps/jopdf.png $out/share/icons/hicolor/256x256/apps/jopdf.png
-        elif [ -f usr/share/icons/hicolor/256x256/apps/jopdf.png ]; then
-          cp usr/share/icons/hicolor/256x256/apps/jopdf.png $out/share/icons/hicolor/256x256/apps/jopdf.png
-        elif [ -f opt/jopdf/jopdf.png ]; then
-          cp opt/jopdf/jopdf.png $out/share/icons/hicolor/256x256/apps/jopdf.png
-        else
-          # Si el paquete no tiene un PNG directo, creamos un fallback visual para que no salga la tuerca rota
-          ln -s $out/opt/jopdf/jopdf.png $out/share/icons/hicolor/256x256/apps/jopdf.png || true
-        fi
+    # Extracción forzada del ícono
+    if [ -f usr/share/pixmaps/jopdf.png ]; then
+      cp usr/share/pixmaps/jopdf.png $out/share/icons/hicolor/256x256/apps/jopdf.png
+    elif [ -f usr/share/icons/hicolor/256x256/apps/jopdf.png ]; then
+      cp usr/share/icons/hicolor/256x256/apps/jopdf.png $out/share/icons/hicolor/256x256/apps/jopdf.png
+    elif [ -f opt/jopdf/jopdf.png ]; then
+      cp opt/jopdf/jopdf.png $out/share/icons/hicolor/256x256/apps/jopdf.png
+    else
+      ln -s $out/opt/jopdf/jopdf.png $out/share/icons/hicolor/256x256/apps/jopdf.png || true
+    fi
 
-        # Lanzador .desktop bien estructurado
-        cat > $out/share/applications/jopdf.desktop <<EOD
+    # Lanzador .desktop bien estructurado
+    cat > $out/share/applications/jopdf.desktop <<EOD
     [Desktop Entry]
     Name=JOPDF
     Comment=JOPDF Free PDF Editor
@@ -69,7 +85,7 @@ stdenv.mkDerivation rec {
     StartupWMClass=JOPDF
     EOD
 
-        runHook postInstall
+    runHook postInstall
   '';
 
   postFixup = "wrapQtApp $out/bin/jopdf";
@@ -79,6 +95,6 @@ stdenv.mkDerivation rec {
     homepage = "https://www.jopdf.com";
     license = lib.licenses.unfree;
     maintainers = [];
-    platforms = ["x86_64-linux"];
+    platforms = [ "x86_64-linux" ];
   };
 }
